9498c9b0d2857dae6eab5ec0c5ea72be9984c1ff
For3stu <70603167+For3stu@users.noreply.github.com>
2022-09-05 20:41:07 +0200

    Version 0.02 Add intro

---
+++ app/src/main/java/com/example/planner/IntroActivity.kt
@@ -0,0 +1,22 @@
+package com.example.planner
+
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class IntroActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+
+        window.insetsController?.hide(WindowInsets.Type.statusBars())
+        supportActionBar?.hide();
+
+
+    }
+}
