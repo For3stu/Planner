1da7406232a5ae40c33fa8667b215bab6a662556
Sylwester.Foerster <xdforest@gmail.com>
2022-09-19 13:50:10 +0200

    SingUpActivity

---
+++ app/src/main/java/com/example/planner/SingUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.planner
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SingUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+    }
+}
